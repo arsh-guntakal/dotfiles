d93ac5d5b7cc36a6c68ae0b9742fd9c56c1fb32d
Arsh Guntakal <arshgunta@gmail.com>
2026-05-10 18:48:16 -0500

    updated lazy-lock. added comment to tmux-copy clarifying usage

--- tmux-copy.sh
+++ tmux-copy.sh
@@ -8,5 +8,6 @@ elif command -v pbcopy > /dev/null; then
 	pbcopy
 else 
 	# Flush stdin and fallback to tmux internal buffer 
+	# if using from remote and want the text, just manually select and copy the normal way
 	cat > /dev/null
 fi
